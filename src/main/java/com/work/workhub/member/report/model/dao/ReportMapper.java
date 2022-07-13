b322e7aa3cdcb33ac65ffb2793324439d6482b48
Areum Jo <chloe1771771@gmail.com>
2022-07-13 18:09:29 +0900

    TO_BE ; myreport userNo 삽입, html 고치기

--- src/main/java/com/work/workhub/member/report/model/dao/ReportMapper.java
+++ src/main/java/com/work/workhub/member/report/model/dao/ReportMapper.java
@@ -1,5 +1,7 @@
 package com.work.workhub.member.report.model.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.work.workhub.member.report.model.dto.ReportDTO;
@@ -7,4 +9,6 @@ import com.work.workhub.member.report.model.dto.ReportDTO;
 @Mapper
 public interface ReportMapper {
 	int registReport(ReportDTO report);
+
+	List<ReportDTO> selectMyList();
 }
